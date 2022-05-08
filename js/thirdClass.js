96e0c2fb150694cb05b6fb83186264eee409c619
kingpin-ay <ayushmondal001@gmail.com>
2022-05-08 16:46:16 +0530

    third day class

---
+++ js/thirdClass.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    // class methods
+    // 1.addClass()
+    // 2.removeClass()
+    // 3.toggleClass()
+    // $('#change').click(function(){
+    //     // $('#test1').addClass("test2");
+    //     // $('#test1').removeClass("test2");
+    //     $('#test1').toggleClass('test2');
+
+    // })
+    
+    // let ayush = {'name' : 'mondal' ,
+    //                 'age' : 24,
+    //             'gender' : 'male'}
+    // console.log(ayush.age)
+    
+    // css methods
+    
+    $('#change').click(function(){
+        
+        $('#test1').css({color: 'black',
+            width: '500px',
+            height: '500px',
+            overflow: 'auto'
+        });
+
+        
+    })
+    
+})
+

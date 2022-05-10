f40094122cd4de72928f373360bec7a8b797df53
kingpin-ay <ayushmondal001@gmail.com>
2022-05-10 19:56:10 +0530

    fourth class without practice

---
+++ js/fourthClass.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  //   $("#btn1").click(function () {
+  //     $("#div1 p").toggleClass("name1");
+  //   }).dblclick(function(){
+  //     $("#div1 p").toggleClass("name2");
+  //   }).contextmenu(function(){
+
+  //   })
+
+  //on and off methods
+  // $("#btn1").on({
+
+  //     'click': function(){
+  //         $("#div1 p").toggleClass("name1");
+  //     }
+
+  //     ,
+
+  //     'dblclick' : function(){
+  //         $("#div1 p").toggleClass("name2");
+  //     }
+  // })
+
+  // $('#btn2').click(function(){
+  //     $("#btn1").off('click');
+  // })
+
+  // append prepend method
+  //   1.append
+  //   2.prepend
+
+  //   $("#btn1").click(function () {
+  //     $("#div1").prepend("<h3>here is our prepend</h3>");
+  //   });
+
+  //   $("#btn1").dblclick(function () {
+  //     $("#div1").prepend("<h3>here is our prepend</h3>");
+  //   });
+
+  // After and Before methods
+  //   1. after
+  //   2. before
+
+  //   $("#btn1").click(function () {
+  //     $("#div1").before("<h3>here is our before </h3>");
+  //   });
+
+  //   $("#btn1").click(function () {
+  //     $("#div1").after("<h3>here is our after</h3>");
+  //   });
+
+  // appendTo & prependTo methods
+    // 1.appendTo
+    // 2.prependTo
+//   $("#btn1").click(function () {
+//     $("<h3>here is our after</h3>").appendTo("#div1");
+//   });
+//   $("#btn1").click(function () {
+//     $("<h3>here is our after</h3>").prependTo("#div1");
+//   });
+
+
+// remove & empty methods
+// 1.remove
+// 2.empty
+
+//   $("#btn1").click(function () {
+//     $('#div1').empty();
+//   });
+
+//   $("#btn1").click(function () {
+//     $('#div1').remove();
+//   });
+
+});
